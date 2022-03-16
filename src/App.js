a5b3280388e2fd295c17c917f650791af4c4c671
Samyak Shah <samyakshah3008@gmail.com>
2022-03-16 12:57:54 +0530

    FEAT: Question10 integrated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Question7 from "./Question7"
 import Question8 from "./Question8/Question8"
 import Question12 from "./Question12"
 import Question11 from "./Question11"
+import Question10 from "./Question10"
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <Question6></Question6>
       <Question7></Question7>
       <Question8></Question8>
+      <Question10></Question10>
       <Question11></Question11>
       <Question12></Question12>
     </div>
